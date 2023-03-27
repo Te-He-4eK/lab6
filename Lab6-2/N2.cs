f725e457b7516c1567d769ed731a512ccb72ed7a
Руслан <@LEGENDA>
2023-03-27 22:30:36 +0300

    Добавьте файлы проекта.

---
+++ Lab6-2/N2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] strings = new string[5];
+
+        // Ввод строк с клавиатуры
+        for (int i = 0; i < strings.Length; i++)
+        {
+            Console.Write($"Введите строку {i + 1}: ");
+            strings[i] = Console.ReadLine();
+        }
+
+        // Вывод строк, содержащих "cat" два раза
+        Console.WriteLine("Строки, содержащие 'cat' два раза:");
+        foreach (string str in strings)
+        {
+            if (str.Contains("catcat") || str.Contains("catandcat"))
+            {
+                Console.WriteLine(str);
+            }
+        }
+
+        // Замена слов, состоящих из 10 и более символов
+        Console.WriteLine("Новые наборы строк:");
+        foreach (string str in strings)
+        {
+            string[] words = str.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length >= 10)
+                {
+                    words[i] = "*";
+                }
+                else if (words[i].Length > 0)
+                {
+                    char firstChar = words[i][0];
+                    words[i] = new string(firstChar, words[i].Length);
+                }
+            }
+            Console.WriteLine(string.Join(" ", words));
+        }
+
+        // Удаление суффиксов "ик"
+        Console.WriteLine("Строки после удаления суффиксов 'ик':");
+        foreach (string str in strings)
+        {
+            string[] words = str.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].EndsWith("ик"))
+                {
+                    words[i] = words[i].Substring(0, words[i].Length - 2);
+                }
+            }
+            Console.WriteLine(string.Join(" ", words));
+        }
+    }
+}
---
+++ lab6/N1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите строку: ");
+        string input = Console.ReadLine();
+        // A
+        if (Regex.IsMatch(input, @"abcd(2023)1{7}0\1"))
+        {
+            Console.WriteLine("Строка соответствует шаблону");
+            input = Regex.Replace(input, "2023", "happynewyear");
+        }
+        else
+        {
+            Console.WriteLine("Строка не соответсвует шаблону");
+        }
+        Console.WriteLine($"Результат a): {input}");
+        // B
+        MatchCollection matches = Regex.Matches(input, @"\d+");
+        List<int> numbers = new List<int>();
+        foreach (Match match in matches)
+        {
+            int number;
+            if (int.TryParse(match.Value, out number))
+            {
+                numbers.Add(number);
+            }
+        }
+        Console.Write("Результат b): ");
+
+        //  сумма
+        int sum = 0;
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            Console.Write(numbers[i]);
+
+            sum += numbers[i];
+
+            if (i < numbers.Count - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine($"\nСумма чисел: {sum}");
+        // макс число и его индекс
+        int max = int.MinValue;
+        int maxIndex = -1;
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] > max)
+            {
+                max = numbers[i];
+                maxIndex = i;
+            }
+        }
+        Console.WriteLine($"Максимальное число: {max}, порядковый номер: {maxIndex + 1}");
+        // С
+        matches = Regex.Matches(input, @"\d+");
+        List<string> decimalNumbers = new List<string>();
+        foreach (Match match in matches)
+        {
+            decimalNumbers.Add(match.Value);
+        }
+        input = Regex.Replace(input, @"\d+", "number");
+        Console.WriteLine($"Результат c): {input}");
+    }
+}
